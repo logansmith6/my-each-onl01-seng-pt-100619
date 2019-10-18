2d67410716db7ee355b6d162e8826b953f4fdab0
Logan Smith <logsmith06@gmail.com>
2019-10-18 22:03:13 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,19 @@
-def my_each # put argument(s) here
+def my_each(array)
   # code here
+
+n = 0
+while n < array.size do
+  yield array[n]
+  n += 1
+
+
+end
+
+return array.to_a
+end
+
+collection = [1,2,3,4]
+my_each(collection) do |i|
+  p i
+  return i
 end
