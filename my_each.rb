41a03977353045e5366269e03c055a40c545d094
Logan Smith <logsmith06@gmail.com>
2019-10-18 21:52:57 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,16 @@
-def my_each # put argument(s) here
+def my_each(array)
   # code here
+size = array.size
+x = 0
+size.times do
+  yield array[x]
+  x += 1
+end
+
+
+end
+
+collection = [1,2,3,4]
+my_each(collection) do |i|
+  puts i
 end
